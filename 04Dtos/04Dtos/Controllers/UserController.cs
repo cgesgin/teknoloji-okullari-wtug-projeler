188f2ac03c26cfdc6b107e6c896fc883aee93ca0
cgesgin <cevher-5@hotmail.com>
2026-02-19 02:29:23 +0300

    Dtos

---
+++ 04Dtos/04Dtos/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using _04Dtos.Dtos;
+using _04Dtos.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _04Dtos.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IMapper _mapper;
+
+        public UserController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            var userDtos = _mapper.Map<List<UserDto>>(_users);
+            return View(userDtos);
+        }
+
+        private List<User> _users = new()
+        {
+            new User{ Id = 1, Username = "Alice", Password = "123", Email = "alice@mail.com"},
+            new User{ Id = 2, Username = "Bob", Password = "456", Email = "bob@mail.com"},
+            new User{ Id = 3, Username = "Charlie", Password = "789", Email = "charlie@mail.com"}
+        };
+    }
+}
---
+++ 04Dtos/04Dtos/Mapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using _04Dtos.Dtos;
+using _04Dtos.Models;
+using AutoMapper;
+
+namespace _04Dtos.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>();
+        }
+    }
+}
